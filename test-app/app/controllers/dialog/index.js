de84ef6377224fb2b5c743e6b65ac59b489a07d0
abeidahmed <abeidahmed92@gmail.com>
2023-04-28 23:02:50 +0530

    add dialog

---
+++ test-app/app/controllers/dialog/index.js
@@ -0,0 +1,15 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class DialogIndexController extends Controller {
+  @tracked isOpen = false;
+
+  @action open() {
+    this.isOpen = true;
+  }
+
+  @action close() {
+    this.isOpen = false;
+  }
+}
--- test-app/app/router.js
+++ test-app/app/router.js
@@ -8,4 +8,5 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('menu', function () {});
+  this.route('dialog', function () {});
 });
