50e781d0ab4e5749f2877d8590ea77a188f12422
Abeid Ahmed <abeidahmed92@gmail.com>
2023-05-08 20:48:48 +0530

    chore(docs): delete unneeded routes (#34)

--- docs/app/router.js
+++ docs/app/router.js
@@ -9,9 +9,4 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   addDocfyRoutes(this);
-  // this.route('examples', function () {
-  //   this.route('dialog');
-  //   this.route('menu');
-  //   this.route('switch');
-  // });
 });
